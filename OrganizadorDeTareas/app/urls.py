ff6bca3d3d24ef4f14da505e3d0ce0626ac34c26
fernandomaron <fernaman@gmail.com>
2019-10-09 19:22:07 -0300

    referrencias entre inicio y usuario

--- OrganizadorDeTareas/app/urls.py
+++ OrganizadorDeTareas/app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from app.views import *
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('user/', userProfile)
 ]
--- OrganizadorDeTareas/app/views.py
+++ OrganizadorDeTareas/app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'LandingPage.html')
+
+def userProfile(request):
+    return render(request, 'UserProfile.html')
