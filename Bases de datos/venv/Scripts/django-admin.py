a2023d3ce6cbec7c3e570b4b87159f49b82cfa96
fernandomaron <fernaman@gmail.com>
2019-10-16 18:57:52 -0300

    Se eliminan archivos innecesarios

--- Bases de datos/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Matias\PycharmProjects\tarea4_ingenieria\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- OrganizadorDeTareas/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/pachon/Desktop/Ingenieria/2019-2-Another-One-Bytes-DDoS-T4/OrganizadorDeTareas/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
